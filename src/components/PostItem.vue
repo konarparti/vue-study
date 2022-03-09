a216ad1552448f4f7dd2aa664a504ed9a2c06910
Anton <anton.gusevv.01@gmail.com>
2022-03-09 22:26:03 +0300

    add post-item vue

--- src/components/PostItem.vue
+++ src/components/PostItem.vue
@@ -1,18 +1,15 @@
 <template>
-  <div>
-    <h3>Список</h3>
-    <post-item :post="post" v-for="post in posts"/>
+  <div class="post">
+    <div><strong>Название: </strong>{{post.title}}</div>
+    <div><strong>Описание: </strong>{{post.body}}</div>
   </div>
-
 </template>
 
 <script>
-import PostItem from "@/components/PostItem";
 export default {
-  components: {PostItem},
-  props:{
-    posts:{
-      type: Array,
+  props: {
+    post: {
+      type: Object,
       required: true,
     }
   }
