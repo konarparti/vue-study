ae4ad83ad82bc1cc584025db5f2672a85965a9b0
Anton <anton.gusevv.01@gmail.com>
2022-03-09 19:37:25 +0300

    base

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <button @click="addLike">Like</button>
+      <button  @click="addDislike">Dislike</button>
+    </div>
+    <div>Количество лайков: <strong>{{likes}}</strong></div>
+    <div>Количество дизлайков: <strong>{{dislikes}}</strong></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return{
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike(){
+      this.likes += 1;
+    },
+    addDislike(){
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+<style>
+div {
+  font-size: 25px;
+}
+</style>
